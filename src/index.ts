5677b8643ef9c22be2f95425c568fd1182cb5530
James M. Greene <jamesmgreene@github.com>
2018-12-17 11:56:02 -0600

    Do you hear what I hear?

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  app.on('*', async (context: Context) => {
+    context.log({ event: context.event, action: context.payload.action })
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
